6835aab5f18f7d1979bf23af9edbd8c5043f5c06
jinho222 <jinho020@yonsei.ac.kr>
2024-03-19 18:29:43 +0900

    update style

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -18,9 +18,9 @@ export default function Header() {
 
   return (
     <Navbar shouldHideOnScroll isBordered>
-      <Link to={'/'}>
+      <Link to={'/'} className='relative after:content-[""] after:right-0 after:bottom-0 after:translate-x-[4px] after:translate-y-[-1px] after:w-[10px] after:h-[10px] after:bg-purple-600 after:rounded-full after:absolute after:z-[-1]'>
         <NavbarBrand>
-          <h1 className="font-bold">Jinoov.dev</h1>
+          <h1 className="font-extrabold">Jinoov.dev</h1>
         </NavbarBrand>
       </Link>
       <NavbarContent className="flex gap-4" justify="center">
