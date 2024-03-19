ff0f960638f815b7985d3d5c21cc9669afce671b
jinho222 <jinho020@yonsei.ac.kr>
2024-03-19 15:26:34 +0900

    update markdown style

--- app/components/markdown.tsx
+++ app/components/markdown.tsx
@@ -1,6 +1,5 @@
-import clsx from 'clsx';
-import styles from './markdown.module.css';
+import css from './markdown.module.css';
 
 export default function Markdown({ content }: { content: string }) {
-  return <div className={clsx(styles['markdown-body'], '[&_ul]:list-disc', '[&_ul]:list-inside')} dangerouslySetInnerHTML={{ __html: content }} />;
+  return <div className={css['post-content']} dangerouslySetInnerHTML={{ __html: content }} />;
 }
